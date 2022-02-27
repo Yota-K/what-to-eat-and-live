137f9bf64192c50186f2c2650222283efdac590f
Y.K <powdersugar828828@gmail.com>
2022-02-28 00:03:48 +0900

    feat: reactQueryの設定変更。

--- src/lib/reactQueryClient.ts
+++ src/lib/reactQueryClient.ts
@@ -3,8 +3,10 @@ import { QueryClient } from 'react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      // デフォルトだと3回まで失敗してもフェッチをする設定になっている
       retry: false,
-      staleTime: 300000,
+      // デフォルトだとユーザーがブラウザのコンポーネントにフォーカスを当てた時に自動でフェッチが動くようになっている
+      refetchOnWindowFocus: false,
     },
   },
 });
