eadb44d1ccddd44ae35b1f21d7c76392ba10ca1b
Y.K <powdersugar828828@gmail.com>
2022-03-06 14:34:42 +0900

    feat: JWTを使った認可処理の実装。

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { ApolloServer } from 'apollo-server-micro';
 import { typeDefs } from '~/apollo/typeDefs';
 import { resolvers } from '~/apollo/resolver/resolvers';
+import { supabase } from '~/lib/supabaseClient';
 
 // この記述がないとJSONで取得できなかった
 export const config = {
@@ -11,7 +12,23 @@ export const config = {
 };
 
 // Apollo Serverを動かすためには、スキーマ定義とリゾルバーが必要
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  async context(args) {
+    const req = args.req as NextApiRequest;
+    const authorization = req.headers.authorization;
+
+    // UserIDの存在チェックは処理ごとに行う
+    const token = authorization?.split(' ')[1] || '';
+    const user = await supabase.auth.api.getUser(token);
+
+    return {
+      currentUserId: user.data?.id,
+    };
+  },
+});
+
 const startServer = server.start();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
