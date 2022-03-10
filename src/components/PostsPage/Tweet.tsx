c6f445def5b638830ba94b975000cbb940f72164
Y.K <powdersugar828828@gmail.com>
2022-03-10 23:21:33 +0900

    feat: つぶやいた投稿がリアルタイムに反映されるようにして。

--- src/components/PostsPage/Tweet.tsx
+++ src/components/PostsPage/Tweet.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
-import { useMutation } from 'react-query';
-import { gql } from 'graphql-request';
-import { Post } from '~/__generated__/graphql';
+import { useQueryClient } from 'react-query';
+import { CreatePostMutation, CreatePostMutationVariables, useCreatePostMutation } from '~/__generated__/graphql';
 import { useQueryState } from '~/lib/hook/useQuery';
 import SelectBox from '~/components/PostsPage/SelectBox';
 import { TweetData } from '~/types/TweetData';
 import { graphqlClient } from '~/lib/graphqlClient';
 
 const Tweet = () => {
+  const queryClient = useQueryClient();
+
   const [tweetData, setTweetData] = useQueryState<TweetData>('tweetData', {
     tweet: '',
     term: {
@@ -33,27 +34,12 @@ const Tweet = () => {
     }
   }, [tweetData]);
 
-  const create = async () => {
-    const query = gql`
-      mutation CreatePost($body: String!, $userId: Int!, $termId: Int!) {
-        createPost(body: $body, userId: $userId, termId: $termId) {
-          success
-          message
-        }
-      }
-    `;
-
-    const res = await graphqlClient.request<{ success: boolean; message: string }>(query, {
-      body: tweetData.tweet,
-      userId: 1,
-      termId: tweetData.term.id,
-    });
-    console.log(res);
-    return {
-      success: res.success,
-      message: res.message,
-    };
-  };
+  const { mutate } = useCreatePostMutation<Error>(graphqlClient, {
+    onSuccess: (data: CreatePostMutation, _variables: CreatePostMutationVariables, _context: unknown) => {
+      queryClient.invalidateQueries('GetPosts');
+      return console.log('mutation data', data);
+    },
+  });
 
   return (
     <>
@@ -69,7 +55,7 @@ const Tweet = () => {
           <button
             className="bg-blue-500 text-white p-2 rounded-3xl disabled:opacity-50 disabled:pointer-events-none"
             disabled={tweet ? false : true}
-            onClick={create}
+            onClick={() => mutate({ body: tweetData.tweet, userId: 1, termId: tweetData.term.id })}
           >
             ツイートする
           </button>
