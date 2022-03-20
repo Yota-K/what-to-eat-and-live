4b657d3bf1ba2b814240bc12a1ec99b08c4b3057
Y.K <powdersugar828828@gmail.com>
2022-03-21 00:30:45 +0900

    feat: つぶやき一覧をSSRにして、ログインしていない時はつぶやけないようにして。

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import { User } from '@supabase/supabase-js';
 import { GetPostsQuery, useGetPostsQuery } from '~/__generated__/graphql';
 import Header from '~/components/Header';
 import Layout from '~/components/Layout';
 import Seo from '~/components/Seo';
 import PostItem from '~/components/PostsPage/PostItem';
 import Tweet from '~/components/PostsPage/Tweet';
+import { supabase } from '~/lib/supabaseClient';
+import { graphqlClient } from '~/lib/graphqlClient';
 import { APP } from '~/config/app';
 
-import { graphqlClient } from '~/lib/graphqlClient';
-const Posts = () => {
+type Props = {
+  user: User;
+};
+
+const Posts: React.FC<Props> = ({ user }) => {
   const { title } = APP;
   const { data, isLoading } = useGetPostsQuery<GetPostsQuery, Error>(graphqlClient);
 
@@ -18,7 +25,7 @@ const Posts = () => {
       <Seo title={`つぶやき一覧｜${title}`} description="今日食べたものをつぶやくことができる画面です" />
       <Header />
       <Layout>
-        <Tweet />
+        {user && <Tweet />}
         {isLoading ? (
           <div>Loaging...</div>
         ) : (
@@ -36,6 +43,16 @@ const Posts = () => {
       </Layout>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
+  // TODO: うまい具合に共通化したい
+  const req = context.req;
+  const { user } = await supabase.auth.api.getUserByCookie(req);
+
+  return {
+    props: { user },
+  };
 };
 
 export default Posts;
